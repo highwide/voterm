1f48cf607e097eb2b47bed888b6691fc51b537d8
highwide <hochweit728@gmail.com>
2017-07-06 12:53:16 +0900

    generator設定

--- config/application.rb
+++ config/application.rb
@@ -18,14 +18,13 @@ Bundler.require(*Rails.groups)
 
 module Voterm
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.system_tests false
+      g.sylesheets   false
+      g.javascripts  false
+      g.helper false
+    end
   end
 end
