6bd36ba95395a9945810e516fd34f41b2d894dbc
highwide <hochweit728@gmail.com>
2017-08-28 19:14:09 +0900

    Fix typo

--- db/migrate/20170709012354_create_rounds.rb
+++ db/migrate/20170709012354_create_rounds.rb
@@ -1,7 +1,7 @@
 class CreateRounds < ActiveRecord::Migration[5.1]
   def change
     create_table :rounds do |t|
-      t.references :votes, foreign_key: true
+      t.references :vote, foreign_key: true
       t.integer :lose_candidacy_id
       t.integer :order_num, null: false
       t.text :report, null: false
