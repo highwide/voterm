199c535c6ae56f02a82f439b20e76e7e39d8d0c2
highwide <hochweit728@gmail.com>
2017-09-06 11:07:58 +0900

    Add validation for presence ballot's name

--- app/controllers/ballots_controller.rb
+++ app/controllers/ballots_controller.rb
@@ -1,22 +1,23 @@
 class BallotsController < ApplicationController
   def new
-    @vote = Vote.find(params[:vote_id])
-    @ballot = Ballot.new
+    @vote = Vote.includes(candidacies: :candidate).find(params[:vote_id])
+    @ballot = @vote.ballots.build
     @ballot.ballot_candidacies.build
-    @candidacies = Candidacy.where(vote_id: params[:vote_id]).includes(:candidate)
   end
 
   def create
-    ballot = Ballot.new(ballot_params.merge(vote_id: params[:vote_id]))
-    ballot.ballot_candidacies = \
+    @vote = Vote.includes(candidacies: :candidate).find(params[:vote_id])
+    @ballot = @vote.ballots.build(ballot_params.merge(vote_id: params[:vote_id]))
+    @ballot.ballot_candidacies = \
       ballot_candidacies_params[:sorted_candidacy_ids]
         .split(',')
         .map
         .with_index { |p, i| BallotCandidacy.new(candidacy_id: p, rank: i + 1) }
-    if ballot.save!
+    if @ballot.save
       redirect_to(vote_path(params[:vote_id]), notice: '投票に成功しました')
     else
-      fail('投票に失敗しました')
+      flash.now[:notice] = '登録に失敗しました'
+      render :new
     end
   end
 
--- app/models/ballot.rb
+++ app/models/ballot.rb
@@ -3,4 +3,6 @@ class Ballot < ApplicationRecord
   has_many :ballot_candidacies, dependent: :destroy
 
   accepts_nested_attributes_for :ballot_candidacies
+
+  validates_presence_of :name
 end
