b48cf246cf8504a13c580231f3e40a79429fbc1b
highwide <hochweit728@gmail.com>
2018-02-25 18:40:07 +0900

    [WIP] modify specs

--- spec/models/ballot_candidacy_spec.rb
+++ spec/models/ballot_candidacy_spec.rb
@@ -3,38 +3,10 @@ require 'rails_helper'
 RSpec.describe BallotCandidacy, type: :model do
   let(:election) { create(:election, :with_vote_and_4candidacies) }
   let(:vote) { election.votes.first }
-  let(:candidacy_ids) { vote.candidacies.pluck(:id) }
+  let!(:ballot) { create(:ballot, :with_ballot_candidacies, vote: vote) }
 
-  describe 'to_calc_params' do
-    subject { BallotCandidacy.to_calc_params(vote.id) }
-    let(:ballot_ids) { create_list(:ballot, 3, vote_id: vote.id).map(&:id) }
-    let(:expected_ary) do
-      [
-        [candidacy_ids[0], candidacy_ids[1], candidacy_ids[2], candidacy_ids[3]],
-        [candidacy_ids[1], candidacy_ids[2], candidacy_ids[3], candidacy_ids[0]],
-        [candidacy_ids[2], candidacy_ids[3], candidacy_ids[0], candidacy_ids[1]],
-      ]
-    end
-
-    before(:each) do
-      create(:ballot_candidacy, ballot_id: ballot_ids[0], candidacy_id: candidacy_ids[0], rank: 1)
-      create(:ballot_candidacy, ballot_id: ballot_ids[0], candidacy_id: candidacy_ids[1], rank: 2)
-      create(:ballot_candidacy, ballot_id: ballot_ids[0], candidacy_id: candidacy_ids[2], rank: 3)
-      create(:ballot_candidacy, ballot_id: ballot_ids[0], candidacy_id: candidacy_ids[3], rank: 4)
-
-      create(:ballot_candidacy, ballot_id: ballot_ids[1], candidacy_id: candidacy_ids[1], rank: 1)
-      create(:ballot_candidacy, ballot_id: ballot_ids[1], candidacy_id: candidacy_ids[2], rank: 2)
-      create(:ballot_candidacy, ballot_id: ballot_ids[1], candidacy_id: candidacy_ids[3], rank: 3)
-      create(:ballot_candidacy, ballot_id: ballot_ids[1], candidacy_id: candidacy_ids[0], rank: 4)
-
-      create(:ballot_candidacy, ballot_id: ballot_ids[2], candidacy_id: candidacy_ids[2], rank: 1)
-      create(:ballot_candidacy, ballot_id: ballot_ids[2], candidacy_id: candidacy_ids[3], rank: 2)
-      create(:ballot_candidacy, ballot_id: ballot_ids[2], candidacy_id: candidacy_ids[0], rank: 3)
-      create(:ballot_candidacy, ballot_id: ballot_ids[2], candidacy_id: candidacy_ids[1], rank: 4)
-    end
-
-    it '同一vote_idの票をrank順にcandidacy_idを並べたballotごとの二重配列になる' do
-      is_expected.to eq expected_ary
-    end
+  describe 'by_vote_id' do
+    subject { BallotCandidacy.by_vote_id(vote.id) }
+    it { expect(subject.sort).to eq ballot.ballot_candidacies.sort }
   end
 end
--- spec/models/result_spec.rb
+++ spec/models/result_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Result, type: :model do
+  describe 'calc' do
+    let(:election) { create(:election, :with_vote_and_4candidacies) }
+    let(:vote) { election.votes.first }
+
+    before(:each) { create(:ballot, :with_ballot_candidacies, vote: vote) }
+
+    it { expect{ Result.calc(vote.id) }.to change{ Result.count }.by(1) }
+  end
 end
